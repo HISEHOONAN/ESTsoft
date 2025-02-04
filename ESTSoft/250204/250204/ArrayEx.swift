56b499d0e433ee3373ecbaaeecd5f56f720c5694
HISEHOONAN <dkstpgns0617@gmail.com>
2025-02-04 17:24:40 +0900

    오류 수정

--- ESTSoft/250204/250204/ArrayEx.swift
+++ ESTSoft/250204/250204/ArrayEx.swift
@@ -92,32 +92,6 @@ struct ArrEx {
     }
     
     func checkTime() {
-        /*
-         배열 요소 관리 문제
-         사용자로부터 5개의 단어를 입력받아 배열에 저장한 후,
-         1. 특정 단어를 검색하여 있는지 확인하고
-         2. 검색된 단어를 삭제한 뒤
-         3. 배열을 출력하는 프로그램을 작성하세요.
-         
-         힌트
-         - readLine()을 사용하여 사용자 입력을 받습니다.
-         - contains(_:)를 사용하여 배열에 특정 단어가 있는지 확인합니다.
-         - remove(at:)을 사용하여 요소를 삭제합니
-         
-         실행 예
-         5개의 단어를 입력하세요:
-         Apple
-         Banana
-         Grape
-         Orange
-         Mango
-         입력한 단어 목록: ["Apple", "Banana", "Grape", "Orange", "Mango"]
-         삭제할 단어를 입력하세요:
-         Grape
-         'Grape'을(를) 삭제하였습니다.
-         최종 단어 목록: ["Apple", "Banana", "Orange", "Mango"]
-        */
-        // 1. 5개의 단어를 입력받아 배열에 저장
         var words: [String] = []
         print("5개의 단어를 입력하세요:")
         for _ in 1...5 {
@@ -126,7 +100,6 @@ struct ArrEx {
             }
         }
         print("입력한 단어 목록: \(words)")
-        // 2. 삭제할 단어 입력 받기
         print("삭제할 단어를 입력하세요:")
         if let deleteWord = readLine() {
             if let index = words.firstIndex(of: deleteWord) {
@@ -136,7 +109,6 @@ struct ArrEx {
                 print("해당 단어가 목록에 없습니다.")
             }
         }
-        // 3. 최종 단어 목록 출력
         print("최종 단어 목록: \(words)")
     }
 }
