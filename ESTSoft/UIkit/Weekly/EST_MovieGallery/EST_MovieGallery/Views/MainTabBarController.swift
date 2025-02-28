47537000664467064cd2dfdcfef94b89c71dffe9
HISEHOONAN <dkstpgns0617@gmail.com>
2025-02-28 15:12:07 +0900

    add WeeklyProj

---
+++ ESTSoft/UIkit/Weekly/EST_MovieGallery/EST_MovieGallery/Views/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  EST_MovieGallery
+//
+//  Created by 안세훈 on 2/28/25.
+//
+
+import UIKit
+
+class MainTabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
